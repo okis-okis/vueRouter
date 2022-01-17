70c65924e1072b5b08f077b635ca2eebb641bba1
okis okis <olegkiselnik@gmail.com>
2022-01-17 16:54:58 +0300

    Initial

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>

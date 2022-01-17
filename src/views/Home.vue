a9a6be207745b93d2b66133d5624fc05b734b1f0
okis-okis <olegkiselnik@gmail.com>
2022-01-17 18:15:10 +0300

    I added main image and text on this image.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <Header />
+    <div class="limit main">
+      <div class="image">
+        <img src="@/images/hiImage.jpg" alt="" />
+      </div>
+      <div class="imageText">
+        <div class="imageText__title">TITLE</div>
+        <div class="imageText__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam
+          et at dolorem? Blanditiis, nobis ducimus vitae modi aspernatur dolor
+          nulla magni ratione repellendus consectetur eum unde nostrum id aut?
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped src="@/styles/home.css" />
 
 <script>
 import Header from "@/components/Header.vue";

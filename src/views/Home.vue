5bd3fd237ac6d78d6220ff3c6c112f0305014730
okis-okis <olegkiselnik@gmail.com>
2022-01-18 15:57:37 +0300

    Added about us block

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="main">
     <Header />
-    <div class="limit main">
+    <div class="limit">
       <div class="image">
         <img src="@/images/hiImage.jpg" alt="" />
       </div>
@@ -11,6 +11,29 @@
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ullam
           et at dolorem? Blanditiis, nobis ducimus vitae modi aspernatur dolor
           nulla magni ratione repellendus consectetur eum unde nostrum id aut?
+        </div>
+      </div>
+      <div class="aboutUs">
+        <div class="aboutUs__title">About Us</div>
+        <div class="aboutUs__content">
+          <div class="aboutUs__image">
+            <img src="@/images/picture.jpeg" alt="" />
+          </div>
+          <div class="aboutUs__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis fuga ea ab id rerum exercitationem provident fugit,
+            necessitatibus delectus optio architecto earum laborum sunt nobis
+            unde porro, reiciendis voluptatibus alias, sint temporibus. Eligendi
+            fuga impedit nulla, similique ullam officia voluptatum assumenda
+            quaerat id commodi neque cumque temporibus consequuntur voluptas
+            ipsam reiciendis laborum totam expedita eum odio doloremque error
+            soluta explicabo est. Praesentium dolore sapiente quam, perferendis
+            ipsam possimus, dolor veritatis eligendi id odio exercitationem
+            deserunt quod explicabo accusantium? Id totam odio delectus, illo,
+            exercitationem laudantium hic fugiat error corrupti architecto
+            aspernatur maiores officiis officia optio ea quidem, ut praesentium
+            quas!
+          </div>
         </div>
       </div>
     </div>
